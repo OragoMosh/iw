07c2ddbc6a6f87ddcf7b409450a8e96929670dcc
Big Bad Waffle <skichenbrand@gmail.com>
2018-08-27 21:51:49 +0200

    implemented getLog and uptime

--- helpers/cli/client.js
+++ helpers/cli/client.js
@@ -1,19 +1,55 @@
-let client = require('scp2');
-let clone = require('../../src/server/misc/clone.js');
+const fs = require('fs');
+const util = require('util');
+const clientScp = require('scp2').scp;
+const clientSsh = require('node-ssh');
+const clone = require('../../src/server/misc/clone.js');
+
+let key = fs.readFileSync('C:/Users/Shaun/.ssh/id_rsa').toString();
 
 module.exports = {
-	exec: async function (options) {
-		let config = clone({}, {
-			host: 'ptr.isleward.com',
-		    username: 'isleward',
-		    privateKey: key,
-		    path: '/home/admin/file.txt'
-		}, options);
+	ssh: null,
 
-		client.scp(config, './', this.onError.bind(this));
+	init: async function (server) {
+		this.ssh = new clientSsh();
+
+		await this.ssh.connect({
+			host: `${server}.isleward.com`,
+			username: 'isleward',
+			port: 4194,
+			privateKey: key
+		});
 	},
 
-	error: function (err) {
+	downloadFile: async function (server, source, target) {
+		let config = clone({}, {
+			host: `${server}.isleward.com`,
+			username: 'isleward',
+			privateKey: key,
+			port: 4194,
+			path: source
+		});
+
+		await util.promisify(clientScp.scp)(config, target);
+	},
+
+	execCommands: async function (commands) {
+		const cbExecCommand = this.execCommand.bind(this);
+
+		return new Promise(async function (res) {
+			let output = '';
+
+			for (let command of commands) 
+				output += await cbExecCommand(command);
+
+			res(output);
+		});
+	},
+
+	execCommand: async function (command) {
+		return (await this.ssh.execCommand(command)).stdout;
+	},
+
+	onError: function (err) {
 
 	}
 };
--- helpers/cli/index.js
+++ helpers/cli/index.js
@@ -1,14 +1,75 @@
+const fs = require('fs');
+const exec = require('child_process').exec;
 let client = require('./client');
 
 let startup = {
 	init: async function (cmd, pars) {
+		if (['uptime'].indexOf(cmd) > -1) 
+			await client.init(pars);
+
 		await this[cmd](pars);
 	},
 
-	downloadLog: async function (pars) {
-		client.exec({
+	uptime: async function (serverName) {
+		let output = await client.execCommands([
+			'forever list --no-colors'
+		]);
 
-		});
+		output = output
+			.split('\n')
+			.pop()
+			.split(' ')
+			.pop()
+			.split(':');
+
+		console.log([
+			'D',
+			'H',
+			'M',
+			'S'
+		].map((m, i) => output[i] + m).join(' '));
+	},
+
+	getLog: async function (serverName) {
+		const targetFilename = `./downloads/${serverName}Log.log`;
+
+		await client.downloadFile(serverName, '.forever/isleward.log', targetFilename);
+
+		let contents = fs
+			.readFileSync(targetFilename)
+			.toString()
+			.split('\n')
+			.filter(c => {
+				return (
+					(
+						c.indexOf('Error Logged:') > -1 ||
+						c.indexOf('ERR_IPC_CHANNEL_CLOSED') > -1 ||
+						c.indexOf('ForbiddenError: Forbidden') > -1 ||
+						(
+							c.indexOf('    at ') > -1 &&
+							c.indexOf('/src/') > -1
+						)
+					) &&
+					c.indexOf('node_modules') === -1
+				);
+			})
+			.map(c => {
+				let res = c
+					.replace('Error Logged: ', '\n')
+					.replace(' UnhandledPromiseRejectionWarning: Error [ERR_IPC_CHANNEL_CLOSED]: channel closed', 'Channel Closed')
+					.replace(')', '')
+					.replace('(node:', '\n')
+					.replace('ForbiddenError: ', '\n');
+
+				res = res.split('/src/server/');
+				res = (res.length > 1) ? res[1] : res[0];
+
+				return res;
+			});
+
+		fs.writeFileSync(targetFilename, contents.join('\n'));
+
+		exec(`"C:/Program Files/Sublime Text 3/sublime_text.exe" ${targetFilename}`);
 	}
 };
 
